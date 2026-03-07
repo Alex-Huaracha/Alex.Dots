ba55831b4bed4eccfabcad166c8a5d3d6c2662a6
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2026-03-07 09:56:07 -0500

    feat(nvim): add nvim-highlight-colors plugin

---
+++ config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,21 @@
+return {
+
+  -- Plugin: nvim-highlight-colors
+  -- URL: https://github.com/brenoprata10/nvim-highlight-colors
+  -- Description: Highlight colors within Neovim
+  {
+    "brenoprata10/nvim-highlight-colors",
+    event = "BufReadPre",
+    opts = {
+      render = "background",
+      enable_hex = true,
+      enable_short_hex = true,
+      enable_rgb = true,
+      enable_hsl = true,
+      enable_hsl_without_function = true,
+      enable_ansi = true,
+      enable_var_usage = true,
+      enable_tailwind = true,
+    },
+  },
+}
